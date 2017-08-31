65d78192e56c107e38d46dcf5627581e35ef4736
John Potts <john@arlingtonchurch.com>
2017-08-31 07:34:08 -0400

    sounds working and title added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 
 $(document).ready(function(){
-  
+  var beep1 = new Audio('audio/beep1.mp3');
+  $('#green').click(function(){
+    beep1.play();
+    console.log("working");
+  });
+  var beep2 = new Audio('audio/beep2.mp3');
+  $('#red').click(function(){
+    beep2.play();
+    console.log("working");
+  });
+  var beep3 = new Audio('audio/beep3.mp3');
+  $('#yellow').click(function(){
+    beep3.play();
+    console.log("working");
+  });
+  var beep4 = new Audio('audio/beep4.mp3');
+  $('#blue').click(function(){
+    beep4.play();
+    console.log("working");
+  });
 
 });
