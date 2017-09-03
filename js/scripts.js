d9e35856ee4c898f2b72c601e2c075a533b33f8d
John Potts <john@arlingtonchurch.com>
2017-09-03 04:22:00 -0400

    more js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,16 @@ var playerTouchesThisRound=[],
 patternToMatch=[],
 round=(patternToMatch.length+1),
 colorObject=["green","red","yellow","blue"];
+var beep1 = new Audio('audio/beep1.mp3');
+var beep2 = new Audio('audio/beep2.mp3');
+var beep3 = new Audio('audio/beep3.mp3');
+var beep4 = new Audio('audio/beep4.mp3');
 
 
+
+
+
+var nextRound = function(){
 var nextColor = colorObject[Math.floor(Math.random()*4+1)];
 
 if (nextColor=="green")
@@ -15,31 +23,43 @@ if (nextColor=="yellow")
 {patternToMatch.push("yellow");}
 if (nextColor=="blue")
 {patternToMatch.push("blue");}
+};
 
+var didTheyLose = function(sequence){
+for (i=0;i<sequence.length;i++){
+  if (sequence[i] != patternToMatch[i])
+  {}   //TODO
+}
+if (sequence.length == patternToMatch.length)
+{}    //TODO
+};
 
-  var beep1 = new Audio('audio/beep1.mp3');
+var playerTurn = function(){
   $('#green').click(function(){
     beep1.play();
     console.log("working");
     playerTouchesThisRound.push("green");
   });
-  var beep2 = new Audio('audio/beep2.mp3');
   $('#red').click(function(){
     beep2.play();
     console.log("working");
     playerTouchesThisRound.push("red");
   });
-  var beep3 = new Audio('audio/beep3.mp3');
   $('#yellow').click(function(){
     beep3.play();
     console.log("working");
     playerTouchesThisRound.push("yellow");
   });
-  var beep4 = new Audio('audio/beep4.mp3');
   $('#blue').click(function(){
     beep4.play();
     console.log("working");
     playerTouchesThisRound.push("blue");
   });
+
+
+
+
+};
+
 
 });
